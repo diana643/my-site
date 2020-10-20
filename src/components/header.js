6e14a3b97b133fa19c4f6599c8558cc13f77969d
sian <diana643643@gmail.com>
2020-10-20 16:39:02 +0200

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Link } from 'gatsby';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    flexGrow: 1,
-    color: '#fff',
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
+    //flexGrow: 1,
+    backgroundColor: '#fff',
+    //opacity: '0.0'
   },
   title: {
-    flexGrow: 1,
+    color: '#000'
   },
 }));
 
@@ -23,25 +22,47 @@ function Header(props) {
   const classes = useStyles();
 
   return (
-    <AppBar position="static" className={classes.root}>
-      <Toolbar color='primary'> 
-        <Typography
-          variant="h6"
-          className={classes.title}
-        >
-          {props.siteTitle}
-        </Typography>
-        <div>
+    <Fragment>
+      <AppBar className={classes.root}>
+      <Toolbar> 
+        <Grid container justify='space-between' alignItems="center"> 
+          <Grid item>
+            <Typography
+            variant="h6"
+            className={classes.title}
+            >
+            Welcome to my site
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Grid container>
+              <Button
+                component={Link}
+                to="/"
+              >
+                Home
+              </Button>
+              <Button
+              component={Link}
+              to="/"
+            >
+              Projects
+            </Button>
             <Button
               component={Link}
               to="/"
-              color="inherit"
             >
-              Home
+              Contact
             </Button>
-        </div>
+            </Grid>
+          </Grid>
+
+        </Grid>
+ 
       </Toolbar>
-    </AppBar>
+      </AppBar>
+    </Fragment>
+    
   );
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,47 @@
-import React from "react"
+import React, { Fragment } from "react"
 import Header from "../components/header"
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import { ThemeProvider } from "@material-ui/core/styles";
-import { theme } from "../assets/theme";
+import Fade from 'react-reveal/Fade';
 
 const useStyles = makeStyles((theme) => ({
-  
+  root:{
+    background: 'rbg(255,255,255)',
+    background: 'linear-gradient(152deg, rgba(255,255,255,1) 46%, rgba(193,215,197,1) 100%)',
+    height: '800px',
+    width: '100%'
+    
+  }
 }));
 
 const Index = (props) => {
   const classes = useStyles();
 
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-      <Header />
-      <div>
-        <Grid container>
-          <h1 style={{fontFamily: 'Raleway'}}>Hello there!This is Sian</h1>
-        </Grid>
-        <Grid container>
-          <Grid item>
-            <h1>Web Development</h1>
+    <div className={classes.root}>
+      <Grid container>
+        <Header />
+      </Grid>
+      <Grid container style={{marginTop:'100px'}}>
+        <Grid item xs={12} sm={6} style={{paddingLeft:'10px'}}>
+          <Grid container>
+            <Fade left>
+            <h1 style={{fontFamily: 'Raleway'}}>Hello there! This is Sian.
+              I come from Beijing, and now based in Paris.
+              I love UI/UX design, Web development.
+              And also I have a music blog and a podcast for fun.</h1>
+            </Fade>
           </Grid>
-          <Grid item>
-            <h1>UI/UX Design</h1>
+          <Grid container justify='flex-end'>
+            <h3>know more about me.</h3>
           </Grid>
-        </Grid>
-        <Grid container>
-          <h1>Podcast</h1>
-        </Grid>
-        <Grid container>
-          <h1>Music reviews</h1>
-        </Grid>
-      </div>
-      
+            
+          </Grid>
+          <Grid xs={12} sm={6}>
+            
+          </Grid>
+      </Grid>
     </div>
-    </ThemeProvider>
-    
   );
 }
 
